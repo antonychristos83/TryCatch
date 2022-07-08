1ae4b0da0de630672f98427a5b2d40c676ad1f9b
antonychristos83 <antonychristos83@gmail.com>
2022-07-08 10:06:28 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryCatch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int[] myNumbers = { 1, 2, 3, 4, 5, 6 };
+                Console.WriteLine(myNumbers[10]);
+            }
+            catch(Exception e) 
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
